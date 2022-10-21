e3af9dbeb6759c82a7cccaab493b5dfbbb7827f9
FZsiga02 <fzsigmond02@gmail.com>
2022-10-21 09:44:01 +0200

    Teszt

--- src/hu/petrik/oop/bankiszolgaltatas/Main.java
+++ src/hu/petrik/oop/bankiszolgaltatas/Main.java
@@ -4,23 +4,39 @@ public class Main {
 
     public static void main(String[] args) {
         Tulajdonos t = new Tulajdonos("Gipsz Jakab");
-        Bank b = new Bank();
-        Szamla sz = new Szamla(t);
-        Kartya k = new Kartya(t, sz, "123456789");
-        HitelSzamla h = new HitelSzamla(t, 200000);
-        MegtakaritasiSzamla m = new MegtakaritasiSzamla(t);
-        BankiSzolgaltatas bsz = new BankiSzolgaltatas(t);
+        MegtakaritasiSzamla m1 = new MegtakaritasiSzamla (t);
+        Szamla szamla = new MegtakaritasiSzamla(t);
+        System.out.println(t.getNev());
+        szamla.befizet(200000);
+        System.out.println("Egyenleg: "+ szamla.getAktualisEgyenleg());
+        szamla.kivesz(100000);
+        System.out.println("Új egyenleg: "+ szamla.getAktualisEgyenleg());
 
-        b.szamlanyitas(t, 100000);
-        b.szamlanyitas(t, 200000);
-        b.szamlanyitas(t, 0);
+        HitelSzamla hitelSzamla = new HitelSzamla(t,500);
+        System.out.printf("Hitelkeret: %d\n", hitelSzamla.getHitelKeret());
+        hitelSzamla.kivesz(450);
+        System.out.printf("Új hitelkeret: %d\n", hitelSzamla.getHitelKeret());
 
-        sz.befizet(300000);
+        Kartya kartya = new Kartya(t,szamla,"123456789");
+        System.out.printf("Kártyaszám: %s\n",kartya.getKartyaSzam());
+        kartya.vasarlas(20000);
+        System.out.printf("Megmaradt összeg: %d\n",szamla.getAktualisEgyenleg());
 
+        HitelSzamla h1 = new HitelSzamla(new Tulajdonos("Gipsz Jakab"),700);
+        HitelSzamla h2 = new HitelSzamla(new Tulajdonos("Gipsz Jakab"),500);
+        MegtakaritasiSzamla m2 = new MegtakaritasiSzamla(new Tulajdonos("Kiss István"));
+        MegtakaritasiSzamla m3 = new MegtakaritasiSzamla(new Tulajdonos("Kovács Béla"));
+        MegtakaritasiSzamla m4 = new MegtakaritasiSzamla(new Tulajdonos("Németh József"));
+        MegtakaritasiSzamla m5 = new MegtakaritasiSzamla(new Tulajdonos("Molnár Aladár"));
+        m4.befizet(20000);
+        m4.setKamat(1000);
+        m4.kamatJovairas();
+        m5.befizet(1500);
+        m5.setKamat(170);
+        m5.kamatJovairas();
 
-        System.out.println(t);
-        System.out.println("Összegyenleg: " + b.getOsszegyenleg(t));
-        System.out.println("Összhitelkeret: " + b.getOsszhitelkeret());
-        System.out.println(b.legnagyobbEgyenleguSzamla(t));
+        Bank bank = new Bank();
+        System.out.println(bank.legnagyobbEgyenleguSzamla(new Tulajdonos("Gipsz Jakab")));
+        System.out.println(bank.getOsszegyenleg(new Tulajdonos("Gipsz Jakab")));
     }
 }
